6512017dc9ced7aa29ed983235da9dcc38ecaf92
BubbleM <1870408473@qq.com>
2018-03-25 16:00:59 +0800

    取消jsx-loader直接使用babel-loader进行编译 并且升级babel-loader版本5 -> 7 解决“Illegal import declaration”

--- src/category/imagemark/imagemark.js
+++ src/category/imagemark/imagemark.js
@@ -1,5 +1,26 @@
-var a = 1;
-var b = 2;
-console.log('.....................');
-console.log(a+b);
-console.log('.....................');
+/* 
+  ImageMark项目入口文件
+*/
+'use strict';
+import {Component} from 'react';
+import ReactDOM from 'react-dom';
+import SildeShow from './../../components/slideshow/js/slideshow';
+
+class ImageMark extends Component{
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+  render(){
+    return(
+      <div className = 'imageMark-wrap'>
+        <SildeShow/>
+      </div>
+    )
+  }
+}
+ReactDOM.render(
+  <ImageMark/>,
+  document.getElementById('app')
+);
+module.exports = ImageMark;
